e1d8d83ebbc1b1bb19ea2c1839e8cffd2a580979
Chris Chen <christinafh.chen@gmail.com>
2018-05-30 14:23:12 +0000

    Done.

--- trip.rb
+++ trip.rb
@@ -1,1 +1,11 @@
 # Code your prompts here!
+puts "Where would you like to stay?"
+stay = gets.chomp.capitalize
+
+puts "What would you like to eat?"
+eat = gets.chomp
+
+puts "how long are you staying?"
+nights = gets.chomp
+
+puts "you are visiting #{stay} for #{nights} nights, so you can eat #{eat}"
