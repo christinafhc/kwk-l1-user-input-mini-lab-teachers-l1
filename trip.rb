a083d4a9bceb0cf7d2d213798757af6b9a492073
Chris Chen <christinafh.chen@gmail.com>
2018-05-30 14:20:44 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -1,1 +1,10 @@
 # Code your prompts here!
+puts "Where would you like to stay?"
+stay = gets.chomp.capitalize
+
+puts "What would you like to eat?"
+eat = gets.chomp
+
+puts "how long are you staying?"
+length = gets.chomp
+
