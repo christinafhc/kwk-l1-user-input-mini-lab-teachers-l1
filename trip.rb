f864abd4a3885fa1f8e6eb128ad5254ccc1e4abf
Chris Chen <christinafh.chen@gmail.com>
2018-05-30 14:22:48 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -8,4 +8,4 @@ eat = gets.chomp
 puts "how long are you staying?"
 nights = gets.chomp
 
-puts "you are visiting #{{stay}} for #{nights} nights, so you can eat #{eat}"
+puts "you are visiting #{stay} for #{nights} nights, so you can eat #{eat}"
